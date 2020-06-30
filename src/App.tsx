d79a04b68ed0f3d8b6ec107b286785669d4eb950
Karen <kngs@ic.ufal.br>
2020-06-30 19:06:39 -0300

    feat: 🎸 add bootstrap and sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 function App() {
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      Hello, World! <Button>Test</Button>
+    </div>
+  );
 }
 
 export default App;
